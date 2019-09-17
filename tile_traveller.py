de958559c16506591a9d49cfb509eb9135589c0e
MyraMidnight <dark_stardragon@hotmail.com>
2019-09-17 11:44:15 +0000

    fixed the loop for asking for directions, if invalid direction

--- tile_traveller.py
+++ tile_traveller.py
@@ -16,6 +16,7 @@ def player_location(row, col) :
         return False
 
 def move_player(current_pos, direction):
+    """Moves the player in specified direction, move_player(current_pos, direction) \n current_pos = [x,y]"""
     new_pos = current_pos
     #north +
     if direction == "n":
@@ -32,9 +33,12 @@ def move_player(current_pos, direction):
     #check if new location is within grid
     if player_location(new_pos[0], new_pos[1]) == True:
         return new_pos
+    else:   
+        return False
 
 #print available directions
 def available_directions(current_pos):
+    """Takes current position ([x,y]) and prints out available directions for the player"""
     print("You can travel:", end=" ")
     #north +
     if player_location(current_pos[0], current_pos[1]+ 1) == True:
@@ -57,9 +61,14 @@ def player_direction(position):
     print()
     #ask user for a direction
     direction = input("Direction: ").lower()
-    #user cannot go outside of the grid
-    if direction in ["s", "n", "e", "w"]:  
-        print(move_player(user_pos, direction))
+    if direction in ["s", "n", "e", "w"]:
+        if move_player(position, direction) != False:
+            #as long as the move is not false/invalid, move the player and ask again
+            player_direction(move_player(user_pos, direction))
+        else: 
+            #asks user again for a direction, staying in same position
+            print('Not a valid direction!')
+            move_player(position, input("Direction: ").lower())
     else: 
         print("Not a valid direction!")
         player_direction(position)
